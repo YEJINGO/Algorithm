068b969192cd1474fbc0b0d09db58d568d2b7688
YEJINGO <114003526+YEJINGO@users.noreply.github.com>
2023-03-23 11:18:47 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int a = 0;
+
+        while (n > 0) {
+            a = n % 10;
+            n /= 10;
+            answer += a;
+
+        }
+        return answer;
+    }
+}
