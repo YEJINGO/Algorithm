b0d7620a24d96388e60ccbc49ff9ca40354cc18e
YEJINGO <114003526+YEJINGO@users.noreply.github.com>
2023-04-07 10:37:30 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -1,11 +1,12 @@
 class Solution {
     public double solution(int[] arr) {
-        double sum = 0;
-        double answer = 0;
+        int sum = 0;
+        double average = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
-        answer = sum / arr.length;
-        return answer;
+        average = (double) sum / arr.length;
+
+        return average;
     }
 }
