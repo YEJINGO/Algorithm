769e68309054b9a89c767081a5b982ed24e8af67
YEJINGO <114003526+YEJINGO@users.noreply.github.com>
2023-04-08 16:45:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.43 ms, Memory: 96 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        Map<String, Integer> map = new HashMap();
+
+        for (int i = 0; i < participant.length; i++) {
+            if (map.containsKey(participant[i])) {
+                map.put(participant[i], map.get(participant[i]) + 1);
+            } else
+                map.put(participant[i], 1);
+        }
+
+        for (int i = 0; i < completion.length; i++) {
+            map.replace(completion[i], map.get(completion[i]) - 1);
+        }
+
+        for (String key : map.keySet()) {
+            if (1 == map.get(key)) {
+                return key;
+            }
+        }
+        return "1";
+    }
+}
