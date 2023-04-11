c47405eca93bca16d1964013d7eca18c32d5c9a4
YEJINGO <114003526+YEJINGO@users.noreply.github.com>
2023-04-11 10:15:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(long num) {
+        if (num == 1) return 0;
+        int count = 0;
+
+        while (num > 1) {
+            if (count >= 500) return -1;
+            else {
+                if (num % 2 == 0) {
+                    num /= 2;
+                } else {
+                    num = (num * 3) + 1;
+                }
+            }
+            count++;
+
+        }
+        return count;
+    }
+}
