31cc477e62dcef4d0a203beda519c25579f80ea1
YEJINGO <114003526+YEJINGO@users.noreply.github.com>
2023-12-19 10:44:11 +0900

    [level 1] Title: 폰켓몬, Time: 8.77 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        int max = nums.length / 2;
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+        if (set.size() > max) {
+            return max;
+        } else {
+            return set.size();
+        }
+    }
+
+}
