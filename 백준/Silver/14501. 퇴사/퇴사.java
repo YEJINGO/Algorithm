576fd53bd6605f166a213cedd05b83fd2970da5b
YEJINGO <114003526+YEJINGO@users.noreply.github.com>
2024-01-11 16:24:25 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] T = new int[N+1];
+		int[] P = new int[N+1];
+		int[] DP = new int[N+2];
+		
+		for(int i=1; i<=N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			T[i] = Integer.parseInt(st.nextToken()); 
+			P[i] = Integer.parseInt(st.nextToken()); 
+		}
+		
+		for(int i = N; i > 0; i--) {
+			int next = i + T[i]; 
+			
+			if(next > N + 1) { 
+				DP[i] = DP[i + 1];
+			} else { 
+				DP[i] = Math.max(DP[i + 1], P[i] + DP[next]);
+			}
+		}
+		
+		System.out.println(DP[1]);
+	}
+
+}
