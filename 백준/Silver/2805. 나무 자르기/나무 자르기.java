d4ac0aff3a57aa816d4b852d37fd464e7dd7394c
YEJINGO <114003526+YEJINGO@users.noreply.github.com>
2024-01-26 10:14:04 +0900

    [Silver II] Title: 나무 자르기, Time: 528 ms, Memory: 119364 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -7,23 +7,27 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer nmToken = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(nmToken.nextToken());
-        int m = Integer.parseInt(nmToken.nextToken());
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int max = 0; 
+
+        st = new StringTokenizer(br.readLine());
         int[] trees = new int[n];
         for (int i = 0; i < n; i++) {
             int treeHeight = Integer.parseInt(st.nextToken());
             trees[i] = treeHeight;
+            max = Math.max(max, trees[i]);
         }
-        Arrays.sort(trees);
 
         int lo = 0;
-        int hi = trees[n - 1]; // 최대 높이의 나무를 기준으로 시작
+        int hi =max;
         int answer = 0;
 
+
         while (lo <= hi) {
+
             int mid = (lo + hi) / 2;
             long sum = 0; // int 형의 범위를 벗어날 수 있으므로 long으로 변경
 
@@ -35,13 +39,13 @@ public class Main {
             }
 
             if (sum >= m) {
-                answer = mid;
+//                answer = mid;
                 lo = mid + 1;
             } else {
                 hi = mid - 1;
             }
         }
 
-        System.out.println(answer);
+        System.out.println(hi);
     }
 }
